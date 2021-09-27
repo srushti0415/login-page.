497bb387db6c81d290dde0b5c72a179945c24158
srushti0415 <srushtikulkarni15@gmail.com>
2021-09-27 11:40:01 +0530

    modified login

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-function login() {
+function login() { 
+    console.log("coming")
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
 
         if(username=="srushti" && password=="12345")
         {
-location.assign("file:///media/fuse/crostini_716aae3388346a01837d507a6fea63559e050640_termina_penguin/login%20page/index1.html")
+location.assign("file:///media/fuse/crostini_716aae3388346a01837d507a6fea63559e050640_termina_penguin/login-page./index1.html")
         }
         else{
             window.alert("Login Failed")
