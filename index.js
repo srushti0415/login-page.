b3593097b9fbb4ba818336eea1319d6a22b96985
srushti0415 <srushtikulkarni15@gmail.com>
2021-11-01 21:28:19 +0530

    updated

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
-function login() { 
-    console.log("coming")
-    var username = document.getElementById('username').value;
-    var password = document.getElementById('password').value;
+// function login() { 
+//     console.log("coming")
+//     var username = document.getElementById('username').value;
+//     var password = document.getElementById('password').value;
 
-        if(username=="srushti" && password=="12345")
-        {
-location.assign("file:///media/fuse/crostini_716aae3388346a01837d507a6fea63559e050640_termina_penguin/login-page./index1.html")
-        }
-        else{
-            window.alert("Login Failed")
-        }
-}
+//         if(username=="srushti" && password=="12345")
+//         {
+// location.assign("file:///media/fuse/crostini_716aae3388346a01837d507a6fea63559e050640_termina_penguin/login-page./index1.html")
+//         }
+//         else{
+//             window.alert("Login Failed")
+//         }
+// }
+
+let a = 1;
+var a = 3;
+
+  {  let a = 2 ;
+    console.log(a);
+  };
+
+// console.log(a); 
